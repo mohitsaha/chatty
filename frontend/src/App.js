2a0324c5eade17f28096b98e59cff0e466ad5267
mohitsaha <ms.saha5764@gmail.com>
2024-01-13 21:18:55 +0530

    login skelton created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-
+import './components/Login'
+import Login from './components/Login';
 function App() {
   return (
     <div className="App">
       <h1>Chat application</h1>
+      <Login></Login>
     </div>
   );
 }
