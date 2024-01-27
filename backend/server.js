477b008db47607cc2d2bbd4455cf4e59fe84d9a6
mohitsaha <ms.saha5764@gmail.com>
2024-01-27 18:16:26 +0530

    chat model created

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,21 @@
-const express = require('express')
+const express = require('express');
+const {chats} = require("./data/data.js")
 const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
 
 app.get("/",(req,res) => {
-    res.send("Api is running")
-})
-
-app.get("api/chat",(req,res)=>{
-    res.send(chats);
+   res.send("chat is working");
+    
 })
 
 app.get("/api/chat",(req,res)=>{
     res.send(chats);
-});
+})
+
+// app.get("/api/chat",(req,res)=>{
+//     res.send(data);
+// });
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT,console.log("server has started"));
--- frontend/src/components/Pages/Chats.js
+++ frontend/src/components/Pages/Chats.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import {useState} from 'react';
+import {useEffect} from 'react';
+import axios from 'axios';
 
 const Chats = () => {
+    const[chats,setChats] = useState([]); 
+    const fetchChats = async () =>{
+        const data = await axios.get("/api/chat");
+        setChats(data);
+    }
+    useEffect(()=>{
+        fetchChats();
+    },[])
   return (
     <div>
-        chats apps
+        {
+            //console.log(chats)
+            chats.data?.map((chat)=><div key={chat._id}>{chat.chatName}</div>)
+        }
     </div>
   )
 }
 
-export default Chats
+export default Chats;
