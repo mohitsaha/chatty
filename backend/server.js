64a6f213a47d3dc69925941de9d9f59e31b2df7c
mohitsaha <ms.saha5764@gmail.com>
2024-02-02 12:35:07 +0530

    jwt working

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,9 @@ const userRoutes = require("./routes/userRoutes.js")
 dotenv.config();
 connectDB();
 const app = express();
+app.use(express.json());
 
-
-app.use('api/user',userRoutes);
-
-// app.get("/api/chat",(req,res)=>{
-//     res.send(data);
-// });
+app.use('/api/user',userRoutes);
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT,console.log("server has started"));
