756a2792b63af8b3e8abc470510b868ec41f5407
mohitsaha <ms.saha5764@gmail.com>
2024-02-02 18:43:54 +0530

    codes fix

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,7 +4,6 @@ const {registerUser} =require("../controllers/userControllers")
 const router = express.Router();
 
 router.route("/").post(registerUser)
-//router.route('/').post(registerUser)
 
 
 module.exports = router;
