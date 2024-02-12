0a7292750108c5ab9ee63a59aecb443b18db604a
mohitsaha <ms.saha5764@gmail.com>
2024-02-12 10:40:13 +0530

    auth and protect middleware created

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express')
-const {registerUser} =require("../controllers/userControllers")
+const {registerUser,authUser,allUsers} =require("../controllers/userControllers")
+const {protect} =require("../middleware/authMid")
 
 const router = express.Router();
 
 router.route("/").post(registerUser)
+router.route("/").get(protect,allUsers);
+
+
 
 
 module.exports = router;
